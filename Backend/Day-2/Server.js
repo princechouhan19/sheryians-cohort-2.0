3a8da4a2f162d6e1c523817e9f4abc7baab2dfd9
Prince Chouhan <princechouhan4606@gmail.com>
2026-01-26 00:01:44 +0530

    feat: initialize Express.js server with basic routes
    Add initial project files for a simple Express.js server:
    - package.json with Express dependency
    - Server.js with root, about, and profile routes
    - .gitignore for node_modules and .env
    - package-lock.json for dependency locking

---
+++ Backend/Day-2/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express() // Server Created
+
+app.get('/' , (req,res)=>{
+    res.send("Hello World!")
+})
+
+app.get('/about' , (req,res)=>{
+    res.send("This Is About Page")
+})
+
+app.get('/profile' , (req,res)=>{
+    res.send("Name = Prince Chouhan")
+})
+
+app.listen(3000) // Server Start 3000 is developement port
